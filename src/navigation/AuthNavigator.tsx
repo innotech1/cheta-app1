0e6b316044cfec4d19351633b19faeec109715a9
innotech1 <innotechintegrated@aol.com>
2026-09-23 12:17:16 +0100

    Fix AuthNavigator imports after moving auth screens to src/screens

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from '../screens/auth/LoginScreen';
-import SignupScreen from '../screens/auth/SignupScreen';
+import LoginScreen from '../screens/LoginScreen';
+import SignupScreen from '../screens/SignupScreen';
 
 export type AuthStackParamList = {
   Login: undefined;
